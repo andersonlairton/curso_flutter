959211c2a2413b2e57dbe95b3fcb8b526d9e39fc
Anderson <Anderson@DESKTOP-QLB9JFC>
2021-07-29 15:01:49 -0300

    Criando classe listascreen e elementos iniciais do layout

---
+++ lista_tarefas_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() { // criando o metodo main
+  runApp(new ListaTarefasApp());
+}
+
+class ListaTarefasApp extends StatelessWidget {
+  // criando metodo da classe,neste caso um widget
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      // criando o materialApp e seus filhos 
+      home: new ListaScreen()
+    );
+  } // classe herdando a outra
+  
+}
+
+// criando outras classes 
+
+class ListaScreen extends StatelessWidget{ // estruturando em partes o layout,em caso de varias telas,especificar cada screen em um arquivo 
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+         appBar: new AppBar(
+           title: new Text('Lista de Tarefas....'),
+         ),
+        body: Column(),
+      );
+  }
+  
+}
